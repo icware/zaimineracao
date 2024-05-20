fbed035940100c41dc92032ed89b27ca5953ad54
Ideilson Souza <ideilson@icware.com.br>
2024-05-20 13:52:44 -0300

    Clear

--- src/config.js
+++
@@ -1,9 +0,0 @@
-
-export const apiAddrres = {
-    address: 'https://zai.mineracao.zabe.com.br',
-    base: 'api',
-    version: 'v1'
-}
-
-export const secretKey = 'qt60O30OK07gNdmdyQoYG2EfLL2K8vjEkcBML4nooN7C06cshx4W1rKKfKwX1yb0';
-
